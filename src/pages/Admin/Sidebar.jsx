323f12ba7949fe7895bc6012ea63040d000e1ceb
Bikash Malu <141820540+Bikash-Malu@users.noreply.github.com>
2024-06-09 10:09:57 +0530

    some changes

--- src/pages/Admin/Sidebar.jsx
+++ src/pages/Admin/Sidebar.jsx
@@ -84,13 +84,13 @@ const Sidebar = ({ children }) => {
               </div>
             </form>
           </div>
-          <div className="relative right-16">
+          <div className="relative right-16 flex items-center">
             <button
               type="button"
               onClick={() => setOpenModal(true)}
               className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-2 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
             >
-              <PiPlus className="inline-block w-4 h-4 mr-2 -mt-2 text-white space-x-2" />
+              <PiPlus className="inline-block w-4 h-4 mr-2 -mt-1 text-white space-x-2" />
               Create Role
             </button>
           </div>
