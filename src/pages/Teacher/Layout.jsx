c8faae181bda1f009e1a70618f749fd3542b2258
Bikash Malu <141820540+Bikash-Malu@users.noreply.github.com>
2024-06-17 17:15:20 +0530

    add

--- src/pages/Teacher/Layout.jsx
+++ src/pages/Teacher/Layout.jsx
@@ -4,12 +4,12 @@ import Navigation from './Navbar';
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen w-[100vw]">
   <Sidebar />
   <div className="flex-1 flex flex-col">
     <Navigation />
     <div className="flex-1 p-1 overflow-y-auto mt-3">
-      <div className="font-semibold p-1 overflow-auto">
+      <div className="font-semibold p-1 max-w-[90vw] max-h-[90vh]">
         {children}
       </div>
     </div>
